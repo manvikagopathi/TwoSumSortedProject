305d0bd145875e172b094427ddf5410c51866dca
manvi <manvikagopathi13@gmail.com>
2024-04-02 10:41:18 -0500

    Initial commit

---
+++ src/main/java/TwoSumSortedSolution.java
@@ -0,0 +1,31 @@
+public class TwoSumSortedSolution {
+    public static void main(String[] args) {
+        int[] numbers = {-2,-7,11,15,12};
+        int target = 10;
+        int[] result = twoSum(numbers, target);
+        System.out.println("Index1: " + result[0] + ", Index2: " + result[1]);
+    }
+
+    public static int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        if (target < -1000 || target > 1000) {
+            throw new IllegalArgumentException("Target value is out of range");
+        }
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
